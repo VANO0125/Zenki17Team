ad4208d9828a35aa6ee5817c5b8e4cbebfd5dcd0
後潟 斗志朗 <gensenya@gmail.com>
2019-02-15 10:11:39 +0900

    プロジェクト追加

---
+++ Team17/Assets/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D rig;
+
+    public float speed;//移動スピード
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any)*speed;//スティックでプレイヤー移動
+    }
+}
