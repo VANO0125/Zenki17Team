3f8501482ca95f41d8c7454dc7ed9f37f47c94fe
後潟 斗志朗 <gensenya@gmail.com>
2019-02-18 11:41:44 +0900

    隕石を掴む、投げる処理の追加

--- Team17/Assets/PlayerCtrl.cs
+++ Team17/Assets/PlayerCtrl.cs
@@ -6,6 +6,8 @@ using GamepadInput;
 public class PlayerCtrl : MonoBehaviour
 {
     private Rigidbody2D rig;
+    private bool isCatch;//隕石を掴んでいるか
+    private GameObject catchMeteo;//掴んでいる隕石
 
     public float speed;//移動スピード
 
@@ -18,6 +20,59 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rig.velocity = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any)*speed;//スティックでプレイヤー移動
+        Vector3 vec = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any);
+        rig.velocity = vec * speed;//スティックでプレイヤー移動
+        //移動方向を向く処理
+        if (vec != Vector3.zero)
+        {
+            Vector3 pos = rig.velocity;
+            float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+            Quaternion rotation = new Quaternion();
+            rotation.eulerAngles = new Vector3(0, 0, angle - 90);
+            transform.rotation = rotation;
+        }
+        MeteoCatch();
+        MeteoThrow();
+    }
+
+    void MeteoCatch()
+    {
+        //Bボタンで前方の隕石を掴む
+        Ray2D catchRay = new Ray2D(transform.position, transform.up); //前方にRayを投射
+        int layerMask = 1 << 8; //Meteoレイヤーにだけ反応するようにする
+        RaycastHit2D meteoHit = Physics2D.Raycast(catchRay.origin, catchRay.direction, 2, layerMask);
+        if (GamePad.GetButtonDown(GamePad.Button.B, GamePad.Index.Any) && catchMeteo == null && meteoHit)
+        {
+            isCatch = true;
+            catchMeteo = meteoHit.transform.gameObject;
+        }
+
+        //掴んだ隕石を止める
+        if (catchMeteo != null)
+        {
+            rig.constraints = RigidbodyConstraints2D.FreezePosition;
+            catchMeteo.transform.parent = transform;
+            catchMeteo.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            catchMeteo.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        else
+        {
+            isCatch = false;
+            rig.constraints = RigidbodyConstraints2D.None;
+            rig.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+    void MeteoThrow()
+    {
+        //Bボタンを離すと前方に隕石を投げる
+        if (isCatch && !GamePad.GetButton(GamePad.Button.B, GamePad.Index.Any))
+        {
+            isCatch = false;
+            catchMeteo.GetComponent<Rigidbody2D>().isKinematic = false;
+            catchMeteo.GetComponent<Rigidbody2D>().AddForce((catchMeteo.transform.position - transform.position).normalized * 50, ForceMode2D.Impulse);
+            catchMeteo.transform.parent = null;
+            catchMeteo = null;
+        }
     }
 }
