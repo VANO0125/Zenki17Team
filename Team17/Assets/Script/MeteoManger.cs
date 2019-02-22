0857925f50daca512177b837ffbd89ea1021ba0b
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2019-02-22 15:27:38 +0900

    隕石発生場所追加

---
+++ Team17/Assets/Script/MeteoManger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoManger : MonoBehaviour
+{
+    public GameObject Meteo;
+    private Transform train;
+    [SerializeField]
+    private List<GameObject> spawnPos;
+    private float timer;
+    public float timermax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnPos();
+        Spawn();
+    }
+
+    void Spawn()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= timermax)
+        {
+            Instantiate(Meteo, train.position, train.rotation);
+
+            timer = 0;
+        }
+    }
+
+    void SpawnPos()
+    {
+        int posNum = Random.Range(0, spawnPos.Count);
+        train = spawnPos[posNum].transform;
+    }
+}
