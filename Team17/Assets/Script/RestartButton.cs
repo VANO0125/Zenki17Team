1fee860d7aa02ef84594be847a8ed3b4937c675c
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2019-05-28 15:21:21 +0900

    タイトルボタン、BGM修正

--- Team17/Assets/Script/RestartButton.cs
+++ Team17/Assets/Script/RestartButton.cs
@@ -19,7 +19,14 @@ public class RestartButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GamePad.GetAnyButton(GamePad.Button.B))
+        //if (GamePad.GetAnyButton(GamePad.Button.B))
+        //0 =B,1=A,2=X,3=Y,7=start
+            if(
+            Input.GetKeyDown(KeyCode.JoystickButton0) ||
+            Input.GetKeyDown(KeyCode.JoystickButton1) ||
+            Input.GetKeyDown(KeyCode.JoystickButton2) ||
+            Input.GetKeyDown(KeyCode.JoystickButton3) ||
+            Input.GetKeyDown(KeyCode.JoystickButton7) )
         {
             button.onClick.Invoke();
         }
